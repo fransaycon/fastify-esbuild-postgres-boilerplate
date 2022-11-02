e689a13c9787d0e91e025d686b81930cea1a5e42
Franrey Anthony S. Saycon <franreysaycon@gmail.com>
2022-11-03 02:53:59 +0800

    feat: add fastify swagger
    Incorporate an automatic API documentation solution with swagger and update route schemas to have
    more sample documentation. Added @fastify/autoload as well for ease of plugin and routes discovery
    as a bonus for this new feature.

--- src/routes/v1/health-routes.ts
+++ src/routes/v1/health-routes.ts
@@ -1,7 +1,24 @@
-export default function healthRoutes(server, options, next) {
-  server.get("/", (req, res) => {
-    res.send({ status: "ok" })
-  })
+export default function healthRoutes(server, _options, next) {
+  server.get(
+    "/",
+    {
+      schema: {
+        description: "Check if API is ok and up and running. ;)",
+        response: {
+          200: {
+            description: "Successful response",
+            type: "object",
+            properties: {
+              status: { type: "string" },
+            },
+          },
+        },
+      },
+    },
+    (_req, res) => {
+      res.send({ status: "ok" })
+    }
+  )
 
   next()
 }
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,21 @@
 import fastify from "fastify"
 import fastifyCorsPlugin from "@fastify/cors"
+import fastifyAutoload from "@fastify/autoload"
 
 import ApiError from "@/components/api-error"
-import knexPlugin from "@/plugins/knexPlugin"
-import healthRoutes from "@/routes/v1/health-routes"
-import itemRoutes from "@/routes/v1/item-routes"
+import path from "path"
 
-function createServer() {
+async function createServer() {
   const server = fastify()
   server.register(fastifyCorsPlugin)
-  server.register(knexPlugin)
 
-  server.register(healthRoutes, { prefix: "/v1" })
-  server.register(itemRoutes, { prefix: "/v1" })
+  server.register(fastifyAutoload, {
+    dir: path.join(__dirname, "plugins"),
+  })
+
+  server.register(fastifyAutoload, {
+    dir: path.join(__dirname, "routes"),
+  })
 
   server.setErrorHandler((error, req, res) => {
     if (error instanceof ApiError) {
